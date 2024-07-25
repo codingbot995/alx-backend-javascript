8ff85439063a224f0aeb2cf877952c27ac9da2da
Jane mathenge <waguthijane61@gmail.com>
2024-07-25 16:55:08 +0000

    unitest

---
+++ 0x06-unittests_in_js/4-payment.js
@@ -0,0 +1,11 @@
+
+const Utils = require('./utils');
+
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+  const rslt = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${rslt}`);
+  return rslt;
+}
+
+module.exports = sendPaymentRequestToApi;
+
